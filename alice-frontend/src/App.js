8db8d987fad7852e1c7f3c5ed6c3a93b1cbbf641
huangdong <huangd@i2r.a-star.edu.sg>
2025-04-01 12:22:41 +0800

    second push2

--- alice-frontend/src/App.js
+++ alice-frontend/src/App.js
@@ -10,7 +10,7 @@ function App() {
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <p>
-        Add something here
+        Add something here22
         </p>
         <a
           className="App-link"
